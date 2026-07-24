7e0b183105ea5669ef66b57c20134ca6370f5afd
John R. Foster, PhD <30320569+JohnRFoster@users.noreply.github.com>
2026-07-24 13:43:44 -0600

    drop Sys.Date, will be tracked by targets

--- R/0_Data.Intake.R
+++ R/0_Data.Intake.R
@@ -6,5 +6,5 @@ library(foundry)
 # my_dataset is set in ~/Documents/.foundry/aliases.yml
 # token and hostnames are set in ~/.Renviron
 df <- datasets.read_table("my_dataset")
-filename <- paste0("Data/CFTEP_7-22_FDE_Survey_Ontology_", Sys.Date(), ".csv")
+filename <- paste0("Data/CFTEP_7-22_FDE_Survey_Ontology.csv")
 readr::write_csv(df, filename)
