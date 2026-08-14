b1268b06de2fb574f2b5faa48e98e4d0622eaa83
John R. Foster, PhD <30320569+JohnRFoster@users.noreply.github.com>
2026-08-14 10:15:56 -0600

    adding packages script for easier debugging and workflow mgmt

---
+++ packages.R
@@ -0,0 +1,16 @@
+# Load required packages ----
+# These are the R packages that are used in the targets pipeline
+
+library(tibble)
+library(plyr)
+library(dplyr)
+library(tidyr)
+library(readr)
+library(stringr)
+library(sp)
+library(raster)
+library(anytime)
+library(lubridate)
+library(geosphere)
+library(sf)
+library(operators)
